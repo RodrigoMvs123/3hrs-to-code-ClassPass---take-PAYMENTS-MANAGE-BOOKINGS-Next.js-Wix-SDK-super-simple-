376b34c5f0be0afdd55513d14306fb5dab77d7c2
Rodrigo M.V.S <88489374+RodrigoMvs123@users.noreply.github.com>
2023-06-30 08:46:01 -0300

    Create Document.js

---
+++ Document.js
@@ -0,0 +1,18 @@
+import { Html, Head, Main, NextScript } from ‘next/document’
+
+const Document = () => {
+       return (
+              <Html lang=“en”>
+                     <head>
+                            <title> Anias ClassPass </title>
+                            <meta name=“viewport” content=“initial-scale=1.”/>
+                     <head/>
+                     <body>
+                            <Main/>
+                            <NextScript/>
+                     </body>
+              </Html>
+       )
+}
+
+export default Document 
